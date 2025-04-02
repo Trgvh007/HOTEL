b72d9313793d9f1a941a5535a77a3de6997bc98a
JPham127 <ptny127@gmail.com>
2025-04-02 16:28:27 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
